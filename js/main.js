79e512c05441032f36d0a01e42668a5c19913f6e
kotoff-antoha <70267948+kotoff-antoha@users.noreply.github.com>
2021-02-10 11:18:25 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -922,7 +922,7 @@ function map(n){
 	var map = new google.maps.Map(document.getElementById('map'), options);
 	var icon={
 		url:'img/icons/map.svg',
-		scaledSize: new google.maps.Size(18, 20),
+		scaledSize: new google.maps.Size(28, 30),
 		anchor: new google.maps.Point(9, 10)
 	}
 	for (var i = 0; i < locations.length; i++) {
